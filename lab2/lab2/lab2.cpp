4a313bfc0e4c1fd940089444ff71c06b71b48a45
Kirill Vorobyev <Kirill2005vorob200@yandex.ru>
2025-03-08 13:50:27 +0400

    add lab1

---
+++ lab2/lab2/lab2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+
+int asm_calc(int a, int b, int c) {
+
+	__asm {
+		mov ax, c; ax = c
+		
+	}
+
+}
+
+int main() {
+
+
+//(2*b - a + b/c)/(c/4 + 1);
+
+	int a, b, c;
+	std::cin >> a;
+	std::cin >> b;
+	std::cin >> c;
+}
+
+
+
